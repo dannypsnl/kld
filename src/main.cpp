21b1e10421aaa375e664242c36461249abd35380
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-10-25 08:14:57 +0800

    use elfio

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,10 @@
 #include <iostream>
 
 using namespace std;
+using namespace ELFIO;
 
 int main(int argc, char **argv) {
+  elfio e{};
   cout << "hello world!" << endl;
   return 0;
 }
--- xmake.lua
+++ xmake.lua
@@ -1,8 +1,9 @@
 add_rules("mode.debug", "mode.release")
 set_languages("c11", "c++17")
 set_warnings("all", "error")
+add_requires("vcpkg::elfio", {alias = "elfio"})
 
 target("kld")
     set_kind("binary")
-    add(find_packages("vcpkg::elfio"))
     add_files("src/*.cpp")
+    add_packages("elfio")
